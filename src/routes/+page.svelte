534c48e58e24e0cb0edfbf779b95020849771ab2
Ian Doering <yungro10@icloud.com>
2026-02-10 22:54:12 +0100

    fix: :bug: scaling window now scales canvas as well

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,11 +6,22 @@
     import { threeScene } from '$lib/components/stores.svelte';
 
   onMount(() => {
+    
+    
     const canvas = document.getElementById("scene") as HTMLCanvasElement;
-
+    
     threeScene.threeScene = new ThreeScene(canvas);
     let scene = threeScene.threeScene.scene;
     
+    window.addEventListener('resize', () => {
+      // Update camera aspect
+      threeScene.threeScene!.camera.aspect = window.innerWidth / window.innerHeight;
+      threeScene.threeScene!.camera.updateProjectionMatrix(); // important!
+
+      // Update renderer size
+      threeScene.threeScene!.renderer.setSize(window.innerWidth, window.innerHeight);
+    });
+
     const earth = new Body(new THREE.Vector3(0, 0, 0), 6.371);
     earth.getBodyObjects().forEach(obj => scene.add(obj));
     threeScene.threeScene.registerBody(earth);
@@ -18,7 +29,7 @@
     const moon = new Body(new THREE.Vector3(38.44, 0, 0), 1.737);
     moon.getBodyObjects().forEach(obj => scene.add(obj));
     threeScene.threeScene.registerBody(moon);
-
+    
     threeScene.threeScene.fitCameraToBounds();
   });
 
